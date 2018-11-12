a15170cefe510d07787044c62ba074e3a4d86317
Joost Flick <joostflick@gmail.com>
2018-11-12 16:31:04 +0100

    parsed javacript

---
+++ d3/bundle.js
@@ -0,0 +1,384 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+module.exports=[
+  {
+    "titel": "De test / Joelle Charbonneau",
+    "language": "dut",
+    "year": "2013",
+    "author": "Charbonneau, Joelle"
+  },
+  {
+    "titel": "Test. / Mirjam Mous",
+    "language": "dut",
+    "year": "2013",
+    "author": "Mous, Mirjam"
+  },
+  {
+    "titel": "De test / Hjorth Rosenfeldt",
+    "language": "dut",
+    "year": "2016",
+    "author": "Rosenfeldt, Hjorth"
+  },
+  {
+    "titel": "Praktijkboek psychologische test : voorbereidende oefeningen / John Wiering",
+    "language": "dut",
+    "year": "2015",
+    "author": "Wiering, John"
+  },
+  {
+    "titel": "Autotest",
+    "language": "dut",
+    "year": "1973",
+    "author": "Author unknown"
+  },
+  {
+    "titel": "Trainingsprogramma voor de psychologische test : aanwijzingen, voorbeelden en oplossingen voor een succesvolle test / Paul Pelshenke",
+    "language": "dut",
+    "year": "2006",
+    "author": "Pelshenke, Paul"
+  },
+  {
+    "titel": "The tickle test / Kathryn White, Adrian Reynolds",
+    "language": "eng",
+    "year": "2017",
+    "author": "White, Kathryn"
+  },
+  {
+    "titel": "Sociaal cognitieve vaardigheden test : handleiding / Teun G. van Manen, Pier J.M. Prins, Paul M.G. Emmelkamp",
+    "language": "dut",
+    "year": "2007",
+    "author": "van Manen, Teun G."
+  },
+  {
+    "titel": "The Times IQ-test : 400 nieuwe oefeningen voor de praktijk / Ken Russell en Philip Carter ; [vert. uit het Engels: Joost Zwart]",
+    "language": "dut",
+    "year": "2002",
+    "author": "Russell, Ken"
+  },
+  {
+    "titel": "The love test / Hugo Hamilton",
+    "language": "eng",
+    "year": "1995",
+    "author": "Hamilton, Hugo"
+  },
+  {
+    "titel": "The test of time / by Garry Kasparov ; transl. [from the Russian] by K.P. Neat",
+    "language": "eng",
+    "year": "1986",
+    "author": "Kasparov, Garri"
+  },
+  {
+    "language": "dut",
+    "year": "2002",
+    "author": "Gijsen, William"
+  },
+  {
+    "titel": "Test je talent / Mireille Geus en Laura Carina Mesland",
+    "language": "dut",
+    "year": "2008",
+    "author": "Geus, Mireille"
+  },
+  {
+    "titel": "Test uw gezondheid / Jannes van Everdingen ... [et al. ; verder werkten mee W. Bergman ... et al. ; eindred. Van den Hoven Bureauredactie ; foto's Consumentenbond, Irene Kievits]",
+    "language": "dut",
+    "year": "2003",
+    "author": "J.J.E. van Everdingen"
+  },
+  {
+    "titel": "Narissa's spannende test / Ella Gray",
+    "language": "dut",
+    "year": "2015",
+    "author": "Gray, Ella"
+  },
+  {
+    "titel": "Testtheorie : inleiding in de theorie van de psychologische test en zijn toepassingen / P.J.D. Drenth, K. Sijtsma",
+    "language": "dut",
+    "year": "2006",
+    "author": "Drenth, P.J.D."
+  },
+  {
+    "titel": "psychologische test voor onderwijs en maatschappij / J. Luning Prak",
+    "language": "dut",
+    "year": "1976",
+    "author": "Prak, Jacob Luning"
+  },
+  {
+    "titel": "Test your EQ / Mark Davis",
+    "language": "eng",
+    "year": "2004",
+    "author": "Davis, Mark"
+  },
+  {
+    "titel": "Test en verbeter uw managementstijl / John Wilson ; [vert. uit het Engels: Katrien Bruyland]",
+    "language": "dut",
+    "year": "2001",
+    "author": "Wilson, John"
+  },
+  {
+    "titel": "TMap NEXT : Business Driven Test Management / Leo van der Aalst ... [et al.]",
+    "language": "dut",
+    "year": "2008",
+    "author": "Leo van der Aalst"
+  },
+  {
+    "titel": "Test en verbeter uw leidinggevende capaciteiten / Brian O'Neill ; [vert. uit het Engels: Moniek Braeckman]",
+    "language": "dut",
+    "year": "2001",
+    "author": "O'Neill, Brian"
+  },
+  {
+    "language": "dut",
+    "year": "1980",
+    "author": "Mesker, P."
+  },
+  {
+    "titel": "The official guide to the TOEFL® test / ETS",
+    "language": "eng",
+    "year": "2015",
+    "author": "Educational Testing Service, Princeton,N.J."
+  },
+  {
+    "titel": "Test en verbeter uw vaardigheden als manager / Vikky Burt ; [vert. uit het Engels: Katrien Bruyland]",
+    "language": "dut",
+    "year": "2001",
+    "author": "Burt, Vikky"
+  },
+  {
+    "titel": "The official guide to the TOEFL test / ETS",
+    "language": "eng",
+    "year": "2009",
+    "author": "Author unknown"
+  },
+  {
+    "titel": "Test uw talenten / Peter Lauster ; [vert. uit het Duits door Parma van Loon ; ill.: Kurt Heger]",
+    "language": "dut",
+    "year": "1991",
+    "author": "Lauster, Peter"
+  },
+  {
+    "titel": "Test je wijnkennis met Hubrecht Duijker : voor iedere wijnliefhebber om spelenderwijs thuis te raken in de wereld van de wijn / [idee & tekst: Hubrecht Duijker ; eindred.: Inmerc]",
+    "language": "dut",
+    "year": "2002",
+    "author": "Duijker, Hubrecht"
+  },
+  {
+    "titel": "Op de proef gesteld : geschiedenis van de psychologische test : tentoonstellingsbrochure / [teksten:  Peter van Drunen en Pieter J. van Strien]",
+    "language": "dut",
+    "year": "1991",
+    "author": "van Drunen, Peter"
+  },
+  {
+    "titel": "Barron's TOEFL iBT : internet-based test / Pamela J. Sharpe",
+    "language": "eng",
+    "year": "2013",
+    "author": "Sharpe, Pamela J."
+  },
+  {
+    "titel": "Vingeroefeningen voor de psychologische test : testvoorbeelden en aanwijzingen voor sollicitanten en liefhebbers / Frank Brenner, Doris Dilger ; [vert. uit het Duits door Rien Pico]",
+    "language": "dut",
+    "year": "1985",
+    "author": "Brenner, Frank"
+  },
+  {
+    "titel": "Stukje bij beestje : test je kennis van de dierenwereld / Lars Gejl",
+    "language": "dut",
+    "year": "2012",
+    "author": "Gejl, Lars"
+  },
+  {
+    "titel": "Drie drama's / Willem Frederik Hermans",
+    "language": "dut",
+    "year": "1962",
+    "author": "Hermans, Willem Frederik"
+  },
+  {
+    "titel": "Een kom water, een test vuur / Hella S. Haasse",
+    "language": "dut",
+    "year": "1959",
+    "author": "Haasse, Hella S."
+  },
+  {
+    "titel": "Pub Quiz Amsterdam : test je kennis in 99 quizvragen / samenstelling Monique den Ouden",
+    "language": "dut",
+    "year": "2016",
+    "author": "Monique den Ouden"
+  },
+  {
+    "titel": "Komt een test bij de dokter / T.A. Boer, S.M.H. Einerhand, J.N. de Haas- de Vries en M.N. van Rijswijk",
+    "language": "dut",
+    "year": "2018",
+    "author": "Boer, T.A."
+  },
+  {
+    "titel": "Test en train uzelf / V. Golenitsjev ; [vert. uit het Russisch: Alexander Münninghoff]",
+    "language": "dut",
+    "year": "1975",
+    "author": "Goleniscev, V.E."
+  },
+  {
+    "titel": "De waarde van de psychologische test / J. Luning Prak",
+    "language": "dut",
+    "year": "1964",
+    "author": "Prak, Jacob Luning"
+  },
+  {
+    "titel": "Test zelf uw rekenkunst / Edward H. Julius ; [vert. uit het Engels door: E.J. van Kervel]",
+    "language": "Language unknown",
+    "year": "1993",
+    "author": "Julius, Edward H."
+  },
+  {
+    "titel": "Practice exercises for the TOEFL : test of English as a foreign language / Pamela J. Sharpe",
+    "language": "eng",
+    "year": "2003",
+    "author": "Sharpe, Pamela J."
+  },
+  {
+    "titel": "Handboek psychodiagnostiek en beperkte begaafdheid : classificatie, test-, schaal- en vragenlijstgebruik / D.W. Kraijer, J.J. Plas",
+    "language": "dut",
+    "year": "2007",
+    "author": "Kraijer, D.W."
+  },
+  {
+    "titel": "Verlaag uw hartleeftijd : test zelf uw hartleeftijd / Lothar Schwarz & Markus Schwarz",
+    "language": "dut",
+    "year": "2011",
+    "author": "Schwarz, Lothar"
+  },
+  {
+    "titel": "The great prostate hoax : how big medicine hijacked the PSA test and caused a public health disaster / Richard J. Ablin, PhD",
+    "language": "eng",
+    "year": "2014",
+    "author": "Ablin, Richard J."
+  },
+  {
+    "titel": "Talentenwijzer : talentontwikkelingsprogramma met online test / Djoerd Hiemstra",
+    "language": "dut",
+    "year": "2018",
+    "author": "Hiemstra, Djoerd"
+  },
+  {
+    "titel": "The electric kool-aid acid test / Tom Wolfe",
+    "language": "eng",
+    "year": "1990",
+    "author": "Wolfe, Tom"
+  },
+  {
+    "titel": "Harde noten : test uw klassieke muziekkennis / [Victor Striker ; red.: De Taallijn]",
+    "language": "dut",
+    "year": "2000",
+    "author": "Striker, Victor"
+  },
+  {
+    "titel": "Stress test : reflections on financial crises / Timothy F. Geithner",
+    "language": "eng",
+    "year": "2014",
+    "author": "Geithner, Timothy F."
+  },
+  {
+    "titel": "Over de grens : de vluchtelingencrisis als reality test / Monika Sie Dhian Ho, René Cuperus & Annelies Pilon (red.)",
+    "language": "dut",
+    "year": "2017",
+    "author": "Monika Sie Dhian Ho"
+  },
+  {
+    "titel": "Markant Nederland : test je toeristische kennis",
+    "language": "dut",
+    "year": "1998",
+    "author": "Author unknown"
+  },
+  {
+    "titel": "The application of the Rorschach test to young children / by Mary Ford",
+    "language": "Language unknown",
+    "year": "1946",
+    "author": "Ford, Mary"
+  },
+  {
+    "titel": "Gentest of geen test? : Hoe de NIPT de zwangerschap verandert / Marcel Zuijderland",
+    "language": "dut",
+    "year": "2017",
+    "author": "Zuijderland, Marcel"
+  },
+  {
+    "titel": "Chill : toptips voor relaxte meiden : test jezelf, maak lekkere snacks en doe iets creatiefs / Leanne Warrick",
+    "language": "dut",
+    "year": "2006",
+    "author": "Warrick, Leanne"
+  },
+  {
+    "titel": "Rails 4 test prescriptions : build a healthy codebase / Noel Rappin",
+    "language": "eng",
+    "year": "2014",
+    "author": "Rappin, Noel"
+  },
+  {
+    "titel": "De grote voetbal-iq-test : daag je vrienden uit! / Willem van Beeren & Harry Hamer",
+    "language": "dut",
+    "year": "2016",
+    "author": "van Beeren, Willem"
+  },
+  {
+    "titel": "Jef Farèl in actie / tekst: G.H. van Maren ; tek.: Ben Horsthuis",
+    "language": "dut",
+    "year": "1996",
+    "author": "Horsthuis, Ben"
+  },
+  {
+    "titel": "Gare dino's en de Grote Kika Cola Test / Tais Teng en Laura van Hoeij",
+    "language": "dut",
+    "year": "2008",
+    "author": "Teng, Tais"
+  },
+  {
+    "titel": "Essential cybersecurity science : build, test, and evaluate secure systems / Josiah Dykstra",
+    "language": "eng",
+    "year": "2015",
+    "author": "Dykstra, Josiah"
+  },
+  {
+    "titel": "Handboek psychodiagnostiek en verstandelijke beperking : classificatie, test- en schaalgebruik / D.W. Kraijer, J.J. Plas ; [ill.: Juul Kraijer]",
+    "language": "dut",
+    "year": "2002",
+    "author": "Kraijer, D.W."
+  },
+  {
+    "titel": "Genees je allergie in een handomdraai : test en therapie met drukpuntmassage / Jimmy Scott ; [ill. Claudia Wagar ; vert. uit het Engels door Margot Bakker]",
+    "language": "dut",
+    "year": "1989",
+    "author": "SCOTT, JIMMY"
+  },
+  {
+    "titel": "Test your English vocabulary in use : pre-intermediate & intermediate / Stuart Redman & Ruth Gairns",
+    "language": "eng",
+    "year": "2003",
+    "author": "Redman, Stuart"
+  },
+  {
+    "titel": "Waarom hij, waarom zij? : test je persoonlijkheid en vind de ware liefde / Helen Fisher",
+    "language": "dut",
+    "year": "2009",
+    "author": "Fisher, Helen"
+  }
+]
+},{}],2:[function(require,module,exports){
+var data = require('./data.json')
+console.log(data)
+
+var maxYear = d3.max(data, function(d) {
+  return d.year
+})
+var minYear = d3.min(data, function(d) {
+  return d.year
+})
+var yearAvg = d3.mean(data, function(d) {
+  return d.year
+})
+
+document.getElementById('heading').innerHTML =
+  'Nieuwste boek komt uit: ' +
+  maxYear +
+  ' en het oudste boek komt uit: ' +
+  minYear +
+  '\n gemiddelde: ' +
+  yearAvg
+
+},{"./data.json":1}]},{},[2]);
