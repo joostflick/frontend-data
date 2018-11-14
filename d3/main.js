7ebb7be533ba6dfcb98c169869e02f882c6df332
Joost Flick <joostflick@gmail.com>
2018-11-14 15:13:00 +0100

    first bar chart using dynamically loaded data

--- d3/main.js
+++ d3/main.js
@@ -1,6 +1,17 @@
 var data = require('./data.json')
 console.log(data)
 
+var languagesCount = d3
+  .nest()
+  .key(function(d) {
+    return d.language
+  })
+  .rollup(function(v) {
+    return v.length
+  })
+  .entries(data)
+console.log(JSON.stringify(languagesCount))
+
 var maxYear = d3.max(data, function(d) {
   return d.year
 })
@@ -11,10 +22,44 @@ var yearAvg = d3.mean(data, function(d) {
   return d.year
 })
 
-document.getElementById('heading').innerHTML =
-  'Nieuwste boek komt uit: ' +
-  maxYear +
-  ' en het oudste boek komt uit: ' +
-  minYear +
-  '\n gemiddelde: ' +
-  yearAvg
+//barchart
+
+const margin = 60
+const width = 1000 - 2 * margin
+const height = 600 - 2 * margin
+
+const svg = d3.select('svg')
+
+const chart = svg
+  .append('g')
+  .attr('transform', `translate(${margin}, ${margin})`)
+
+const yScale = d3
+  .scaleLinear()
+  .range([height, 0])
+  .domain([0, languagesCount[0].value + 50])
+
+chart.append('g').call(d3.axisLeft(yScale))
+
+const xScale = d3
+  .scaleBand()
+  .range([0, width])
+  .domain(languagesCount.map(d => d.key))
+  .padding(0.2)
+
+chart
+  .append('g')
+  .attr('transform', `translate(0, ${height})`)
+  .call(d3.axisBottom(xScale))
+
+chart
+  .selectAll()
+  .data(languagesCount)
+  .enter()
+  .append('rect')
+  .attr('x', s => xScale(s.key))
+  .attr('y', s => yScale(s.value))
+  .attr('height', s => height - yScale(s.value))
+  .attr('width', xScale.bandwidth())
+
+  .attr('x', (actual, index, array) => xScale(actual.key))
--- index.js
+++ index.js
@@ -8,36 +8,37 @@ const client = new OBAWrapper({
 })
 client
   .get('search', {
-    q: 'test',
+    q: 'website',
     refine: true,
     facet: 'type(book)',
-    count: 60
+    count: 5000
   })
   .then(results => {
+    console.log(JSON.stringify(results))
     var boeken = results.map(book => createBookInstance(book))
+    var cleanedBoeken = boeken.filter(book => book.year != null)
     fs.writeFile(
       'd3/data.json',
-      JSON.stringify(boeken, null, '  '),
+      JSON.stringify(cleanedBoeken, null, '  '),
       'utf8',
       function() {}
     )
   })
-
 function createBookInstance(book) {
   bookInstance = {
-    titel: book.titles.title.$t,
+    // titel: book.titles.title.$t,
     language:
       !book.languages || !book.languages.language
         ? 'Language unknown'
         : book.languages.language.$t,
     year:
       !book.publication || !book.publication.year
-        ? 'Year unknown'
-        : book.publication.year.$t,
-    author:
-      !book.authors || !book.authors['main-author']
-        ? 'Author unknown'
-        : book.authors['main-author'].$t
+        ? null
+        : Number(book.publication.year.$t)
+    // author:
+    //   !book.authors || !book.authors['main-author']
+    //     ? 'Author unknown'
+    //     : book.authors['main-author'].$t
   }
   return bookInstance
 }
